cb0f9745e90f919de35069930c1d7fdc5cd4edb4
ochaocha01 <ocha50241230@icloud.com>
2024-10-25 22:12:32 +0900

    てすと

---
+++ お餅伝説２/Camera.cpp
@@ -0,0 +1,5 @@
+#include "Camera.h"
+#include "DxLib.h"
+
+
+
--- お餅伝説２/main.cpp
+++ お餅伝説２/main.cpp
@@ -5,9 +5,19 @@
 #include "mobchara.h"
 #include "fight.h"
 #include "fps.h"
+#include"MainScreen.h"
+#include "Camera.h"
 
-const int displaywidth = 1920;
-const int displayhight = 1080;
+
+int displaywidhtX = 0;
+int displayhigthY = 0;
+
+ int displaywidth2X = 1920;
+ int displayhight2Y = 1080;
+
+ int MeinscreenVertical = 1080;
+ int MeinscreenHorizontal = 1920;
+ 
 
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
@@ -15,14 +25,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	
 	// DxLib初期化
 	ChangeWindowMode(TRUE);
-	SetGraphMode(displaywidth, displayhight, 16);
-	mobcharaimg();
+
+	//スクリーンがあった場所
+	//SetGraphMode(displaywidth, displayhight, 16);
+	camera.Screen(displaywidhtX, displayhigthY, displaywidth2X,displayhight2Y);
+
 	if (DxLib_Init() == -1)
 	{
 		return -1;
 	}
 	
-	PlayerInit(displaywidth,displayhight);
+	
+
+	PlayerFunctionOll.PlayerInit(displaywidth2X, displayhight2Y);
+
 	mapInit();//マップの初期化
 	
 	MapEngine();
@@ -33,6 +49,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	// ゲームループ
 	//FPS初期化
 	FPSInit();
+	int Exchange = 0;
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
 		// deltaTime計測
@@ -41,21 +58,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		deltaTime = (nowCount - prevCount) / 1000.0f;
 
 		PlayeAttackType Playerchoicetern(); //プレイヤーの攻撃パターン構造体
+		
 
-
-		switch (int Exchange = 1)
+		switch (Exchange)
 		{
 		case 0://メインスクリーン
+			
+			Mein.MeinDrawScreen(&Exchange);
 
+			break;
+			
 		case 1:
 			
 			ClearDrawScreen();		//マップの描画
 			//MapEngine();
 			mapaupdate();			//マップの動き
 			
-					//
+			
 
-			Playerappdate();		//プレイヤーの更新
+			PlayerFunctionOll.Playerappdate();		//プレイヤーの更新
 
 			
 			mapDraw();	
@@ -63,8 +84,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			
 						//画面更新処理
 			
-			PlayerDraw();			//プレイヤー描画
+			PlayerFunctionOll.PlayerDraw();			//プレイヤー描画
 			
+
 			break;
 
 		}
@@ -78,11 +100,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		}
 	
 	}
-	PlayerFinalize();	//プレイヤーの後始末
+	PlayerFunctionOll.PlayerFinalize();	//プレイヤーの後始末
 	WaitKey();
 	DxLib_End();
 	return 0;
 }
 
-//テスト
-//テスト
